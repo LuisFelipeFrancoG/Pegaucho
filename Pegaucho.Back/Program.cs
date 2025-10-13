24b7280617e70b28bee26a894453587d76915f43
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-10-13 15:39:09 -0500

    Entidad Producto

--- Pegaucho.Back/Program.cs
+++ Pegaucho.Back/Program.cs
@@ -1,16 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+// Inyecciones de dependencias
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Midlewares
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
---
+++ Pegaucho.Shared/Entities/ProductoInventario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pegaucho.Shared.Entities
+{
+    public class ProductoInventario
+    {
+        private int Id  { get; set; }
+
+        [Display(Name = "Nombre del Producto")]
+        [Required]
+        [MaxLength(50)]
+        private string Nombre { get; set; }
+
+        [Display(Name = "Descripcion del Producto")]
+        [Required]
+        [MaxLength(200)]
+        private string Descripcion { get; set; }
+
+        [Display(Name = "Unidad de Medida del Producto")]
+        [Required]
+        [MaxLength(20)]
+        private string UnidadMedida { get; set; }
+
+        [Display(Name = "Costo Unitario del Producto")]
+        [Required]
+        [MaxLength(100)]
+        private double CostoUnitario { get; set; }
+
+}
+}
