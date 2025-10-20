a05d8be73490582955cf9275375b48c13ed1f905
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-10-20 09:35:59 -0500

    Enums

---
+++ Pegaucho.Shared/Entities/Accion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pegaucho.Shared.Entities
+{
+    public enum Accion
+    {
+        CREAR_ORDEN,
+        MODIFICAR_ORDEN,
+        CAMBIAR_ESTADO_ORDEN,
+        ELIMINAR_ORDEN,
+        INICIAR_TAREA,
+        FINALIZAR_TAREA,
+        REPORTAR_ESTADO,
+        REGISTRAR_INCIDENTE,
+        VERIFICAR_PRODUCTO  
+    }
+}
---
+++ Pegaucho.Shared/Entities/EstadoTarea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pegaucho.Shared.Entities
+{
+    public enum EstadoTarea
+    {
+        PENDIENTE,
+        EN_PROCESO,
+        FINALIZADA,
+        CANCELADA
+    }
+}
